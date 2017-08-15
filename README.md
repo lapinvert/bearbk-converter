382f6f2625f833058b30c20f17d380396852c1be
Remy <lapin.vert@gmail.com>
2017-08-15 23:18:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Bearbk Converter
+
+`bearbk-converter` is a quick utility that allows you to convert Bear Writer's backup files ".bearbk" into folder including all your notes as ".txt" or ".md" files, with correct creation/modification dates.
+As a secondary option, `bearbk-converter` can also automatically version your notes with git, for those of you who want to be able to keep track of your files history.
+
+## Dependencies
+
+- jq -> Install with `brew install jq`
+
+## Global Installation
+
+`cp bearbk-converter.sh /usr/local/bin/bearbk-converter`
+
+`chmod +x /usr/local/bin/bearbk-converter`
+
+## Basic Usage
+
+Global usage : `bearbk-converter path/to/backup.bearbk`
+Local usage :  `bash bearbk-converter.sh path/to/backup.bearbk`
+
+This will output a "BEAR_BACKUP" folder next to the archive, including all your notes as .txt, with correct creation date and modification dates.
+
+** Warning ** : It WONT work if there is spaces in path and or bearbk archive file name. You may have to rename it to make it work. See known limitations.
+
+## Options
+
+`--markdown` : To output ".md" files instead of ".txt" files
+
+`--gitpath=/path/to/gitfolder` : Instead of creating a BEAR_BACKUP folder, it will move all your converted notes into the local git repository provided, and perform an automatic commit. Commit name will look like "Update 20170308" (with current date of course).
+
+## Example
+
+`bearbk-converter --markdown --gitpath=/path/to/gitfolder /path/to/archive.bearbk`
+
+## Tested environments
+
+- MAC OS 10.11.6, bash 3.2
+
+## Known Limitations
+
+- Do not work when there is space in archive name.
+- Final file names after conversion may include a number at the end, if there was a identical filename within the trash.
+- Image links are broken and we should replace [assets/file] by ![](assets/file) to make it work in any MarkDown reader.
+
+## Your help is needed
+
+Let me know your thought. Please fill issues if you find, or request changes, or help me maintaining it.
+
+- Deploying the script in and/or npm homebrew for easy install
+- For testing in more environments
+- For being able to take tags into account and maybe separate notes by tags
---
+++ bearbk-converter.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+set -e ## The script should stop on error
+
+## HELPER FUNCTIONS
+help_error() {
+    if [ $# -gt 0 ]; then echo $1; fi
+    echo 'Basic usage : bw-backup-converter [options] path/to/archive'
+    echo 'Available options are :'
+    echo '    --markdown to output .md files instead of .txt'
+    echo '    -g --gitpath to specify the local path to a git repository to which the backed up data will be automatically moved, commited and pushed.'
+    exit 1
+}
+
+is_set() {
+    ! [ -z ${1+x} ]
+}
+is_directory() {
+    [ -d "$1" ]
+}
+
+file_exists() {
+    [ -f "$1" ]
+}
+
+## Ensuring external dependencies are available
+type jq >/dev/null 2>&1 || { echo >&2 "Error : You should install 'jq' before you can run this script. Try 'brew install jq'."; exit 1; }
+type bsdtar >/dev/null 2>&1 || { echo >&2 "Error : You should install 'bsdtar' before you can run this script. "; exit 1; }
+type sed >/dev/null 2>&1 || { echo >&2 "Error : You should install 'sed' before you can run this script. "; exit 1; }
+
+## Check arguments were provided
+if [[ $# -eq 0 ]] ; then
+    help_error 'Error : You have to specify the path to the Bear Archive.'
+fi
+
+## Serialize arguments
+for i in "$@"
+do
+    case $i in
+        -g=*|--gitpath=*)
+        GITPATH="${i#*=}"
+        shift # past argument=value
+        ;;
+        --markdown)
+        IS_MARKDOWN=YES
+        shift # past argument=value
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+done
+
+## Basic variable assignments and error checks
+EXEC_LOCATION=`pwd`
+
+if is_set $GITPATH && ! is_directory "$GITPATH"/".git"; then help_error "Error : This is not a git repository"; fi
+
+BW_ARCHIVE_ARGUMENT=$1
+if ! file_exists $BW_ARCHIVE_ARGUMENT; then help_error "Error : Archive File Path is not valid"; fi
+
+BW_ARCHIVE_LOCATION=$(dirname "$BW_ARCHIVE_ARGUMENT")
+BW_ARCHIVE_FILE=$(basename "$BW_ARCHIVE_ARGUMENT")
+BW_ARCHIVE_EXTENSION="${BW_ARCHIVE_FILE##*.}"
+BW_ARCHIVE_FILENAME="${BW_ARCHIVE_FILE%.*}"
+
+## Ensuring .bearbk files is passed as first argument.
+if [ $BW_ARCHIVE_EXTENSION != "bearbk" ]; then
+    echo "Error : You should specify a bear writer archive '.bearbk'";
+    exit 1;
+fi
+
+## Ensuring passed file is a Zip archive.
+if [[ `file "$BW_ARCHIVE_ARGUMENT"` != *"Zip"* ]]; then
+  echo "Error : This is not a proper zip archive";
+  exit 1;
+fi
+
+TMP_DIR_NAME="BEAR_BACKUP";
+
+## Moving to the archive location
+cd "$BW_ARCHIVE_LOCATION";
+
+## Create temporary work directory or exit
+mkdir $TMP_DIR_NAME 2>/dev/null || { echo "A folder named $TMP_DIR_NAME already exists. Abording."; exit 1; }
+
+## Unzip the archive content in temporary folder (without main container)
+bsdtar -xf "$BW_ARCHIVE_FILE" -s'|[^/]*/||' -C $TMP_DIR_NAME;
+
+for SUBFOLDER in $TMP_DIR_NAME/*; do
+    [ -e "$SUBFOLDER" ] || continue;
+
+    ## Exit if not expected files
+    [ ! -e "$SUBFOLDER"/"info.json" ] && echo "Error : The file info.json doesn't exist" && exit 1;
+    [ ! -e "$SUBFOLDER"/"text.txt" ] && echo "Error : The file text.txt doesn't exist" && exit 1;
+
+    ## Skip if trashed
+    IS_TRASHED=$(cat "$SUBFOLDER""/info.json" | jq '.["net.shinyfrog.bear"].trashed' );
+    if [ $IS_TRASHED == "null" ]; then echo "Unable to trash variable. Abording."; exit 1; fi;
+    if [ $IS_TRASHED == "1" ]; then echo "trashed"; rm -rf "$SUBFOLDER"; continue; fi;
+
+    ## Get file's infos
+    TITLE=$(basename "${SUBFOLDER%.*}");
+    MODIFICATION_DATE=$(cat "$SUBFOLDER""/info.json" | jq '.["net.shinyfrog.bear"].modificationDate' | sed -e 's/^"//' -e 's/"$//'); # Sed to get rid of quotes otherwise date doesn't work
+    CREATION_DATE=$(cat "$SUBFOLDER""/info.json" | jq '.["net.shinyfrog.bear"].creationDate' | sed -e 's/^"//' -e 's/"$//');
+
+    ## Reformat dates for passing to "touch"
+    MOD_DATE_REFORMATED=$(date -j -f %Y-%m-%dT%H:%M:%S%z $MODIFICATION_DATE +%Y%m%d%H%M);
+    CREA_DATE_REFORMATED=$(date -j -f %Y-%m-%dT%H:%M:%S%z $CREATION_DATE +%Y%m%d%H%M);
+
+    ## Set Creation and Modification dates
+    touch -t $CREA_DATE_REFORMATED "$SUBFOLDER""/text.txt"; # Changes both creation date and modification date
+    touch -t $MOD_DATE_REFORMATED "$SUBFOLDER""/text.txt"; # Changes only modificaiton date
+
+    ## Prepare file extension
+    if is_set $IS_MARKDOWN; then FILE_EXTENSION="md"; else FILE_EXTENSION="txt"; fi
+
+    ## In case the file has image
+    if is_directory "$SUBFOLDER"/assets; then
+        ## Rename file
+        mv "$SUBFOLDER"/"text.txt" "$SUBFOLDER"/"$TITLE".$FILE_EXTENSION
+        ## Create wrapper folder
+        mkdir "$TMP_DIR_NAME"/"$TITLE"
+        ## Move files into the new folder
+        mv "$SUBFOLDER"/* "$TMP_DIR_NAME"/"$TITLE"
+    else
+        ## Rename and change location
+        mv "$SUBFOLDER"/"text.txt" "$TMP_DIR_NAME"/"$TITLE".$FILE_EXTENSION
+    fi
+    ## Delete subfolder
+    rm -rf "$SUBFOLDER";
+
+
+done
+
+## Go back to starting point
+cd "$EXEC_LOCATION";
+
+## Commit on Git
+if is_set $GITPATH; then
+
+    cp -f "$BW_ARCHIVE_LOCATION"/"$TMP_DIR_NAME"/* "$GITPATH"
+
+    cd "$GITPATH";
+    git add -A;
+    git commit -m"Update `date +%Y-%m-%d`";
+    cd "$EXEC_LOCATION";
+    rm -R "$BW_ARCHIVE_LOCATION"/"$TMP_DIR_NAME"
+
+fi
+
+exit
